330088c5fcfb7993a510fcfa8745825adc72f8cb
Zach Ancona <zancona@rstudio03.research.sesync.org>
2017-07-20 16:08:36 -0400

    Moved LULC to R folder

---
+++ Scripts/R/LULC.R
@@ -0,0 +1,22 @@
+library(sf)
+library(rgdal)
+library(raster)
+library(dplyr)
+
+DATADIR <- '/nfs/NaturalCapitalAccounting-data'
+
+shp <- file.path(DATADIR, 'cb_2016_us_county_5m')
+counties <- st_read(shp, stringsAsFactors = FALSE)
+
+raster_2001 <- file.path(DATADIR, 'Land_Use_and_Land_Change/LC_2001_MD_Clip.tif')
+#nlcd_2001 <- raster(raster_2001)
+raster_2006 <- file.path(DATADIR, 'Land_Use_and_Land_Change/LC_2006_MD_Clip.tif')
+raster_2011 <- file.path(DATADIR, 'Land_Use_and_Land_Change/LC_2011_MD_Clip.tif')
+nlcd_stack <- stack(c(raster_2001,raster_2006,raster_2011))
+nlcd_stack <- aggregate(nlcd_stack, fact = 10, fun = modal)
+
+counties_md <- filter(counties, STATEFP == '24')
+
+forest <- (nlcd_stack %in% c(41,42,43))
+
+nlcd_md <- extract(nlcd_stack, counties_md, fun = sum)
